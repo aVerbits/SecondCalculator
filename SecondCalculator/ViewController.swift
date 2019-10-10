dc68ba999f2418834cc282c30d9db213caf00741
aVerbits <56268791+aVerbits@users.noreply.github.com>
2019-10-10 17:53:03 +0300

    Initial Commit

---
+++ SecondCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondCalculator
+//
+//  Created by user on 10.10.2019.
+//  Copyright © 2019 alekseiverbitski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
