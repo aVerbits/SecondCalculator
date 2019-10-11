e2a99ed2713c198932f2c5f2c5cc4932c75edfb2
aVerbits <56268791+aVerbits@users.noreply.github.com>
2019-10-11 16:09:52 +0300

    operrate With Two Operands

--- SecondCalculator/ViewController.swift
+++ SecondCalculator/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayResaltLabel: UILabel!
     var stillTyping = false
-    
+    var dotIsPlaced = false
     var firstOperand: Double = 0
+    var secondOperand: Double = 0
+    var operationSign: String = ""
+
+    
     var currentInput: Double {
         get {
             return Double(displayResaltLabel.text!)!
@@ -38,13 +42,72 @@ class ViewController: UIViewController {
         }
       }
     @IBAction func twoOperandSignPressed(_ sender: UIButton) {
-        
+        operationSign = sender.currentTitle!
         firstOperand = currentInput
-        print(firstOperand)
         stillTyping = false
-        
+        dotIsPlaced = false
     }
     
+    func operrateWithTwoOperands(operation: (Double, Double) -> Double) {
+        currentInput = operation(firstOperand, secondOperand)
+        stillTyping = false
+        dotIsPlaced = false
     }
+    
+    @IBAction func equalitySignPressed(_ sender: UIButton) {
+        
+        if stillTyping {
+            secondOperand = currentInput
+        }
+        dotIsPlaced = false
+        
+        switch operationSign {
+        case "+":
+            operrateWithTwoOperands{$0 + $1}
+        case "-":
+            operrateWithTwoOperands{$0 - $1}
+        case "×":
+            operrateWithTwoOperands{$0 * $1}
+        case "÷":
+            operrateWithTwoOperands{$0 / $1}
+
+        default: break
+        }
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        firstOperand = 0
+        secondOperand = 0
+        currentInput = 0
+        displayResaltLabel.text = "0"
+        stillTyping = false
+        dotIsPlaced = false
+        operationSign = ""
+    }
+    @IBAction func plusMinusButtonPressed(_ sender: UIButton) {
+        currentInput = -currentInput
+    }
+    @IBAction func persentegButtonPressed(_ sender: UIButton) {
+        if firstOperand == 0 {
+            currentInput = currentInput / 100
+        }else {
+            secondOperand = firstOperand * currentInput / 100
+        }
+    }
+    @IBAction func squareRootButtonPressed(_ sender: UIButton) {
+        currentInput = sqrt(currentInput)
+    }
+    @IBAction func dottButtonPressed(_ sender: UIButton) {
+        if stillTyping && !dotIsPlaced {
+            displayResaltLabel.text = displayResaltLabel.text! + "."
+            dotIsPlaced = true
+        }else if !stillTyping && !dotIsPlaced {
+            displayResaltLabel.text = "0."
+        }
+    }
+    
+    
+    
+}
 
 
