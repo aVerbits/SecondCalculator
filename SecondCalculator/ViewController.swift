218d5b31e93585822790cfb0b50f171b93cba926
aVerbits <56268791+aVerbits@users.noreply.github.com>
2019-10-11 13:34:03 +0300

    Number pressed

--- SecondCalculator/ViewController.swift
+++ SecondCalculator/ViewController.swift
@@ -9,17 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var displayResaltLabel: UILabel!
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+    }
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
 
