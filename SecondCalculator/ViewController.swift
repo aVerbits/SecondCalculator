802e515da8e5920ba14df5324b91e3b76bd979a5
aVerbits <56268791+aVerbits@users.noreply.github.com>
2019-10-11 14:49:28 +0300

    Operandes and "+"

--- SecondCalculator/ViewController.swift
+++ SecondCalculator/ViewController.swift
@@ -11,8 +11,38 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayResaltLabel: UILabel!
+    var stillTyping = false
+    
+    var firstOperand: Double = 0
+    var currentInput: Double {
+        get {
+            return Double(displayResaltLabel.text!)!
+        }
+        set {
+            displayResaltLabel.text = "\(newValue)"
+            stillTyping = false
+        }
+    }
     
     @IBAction func numberPressed(_ sender: UIButton) {
+        
+        let number = sender.currentTitle!
+        
+        if stillTyping {
+            if (displayResaltLabel.text?.characters.count)! < 20 {
+                displayResaltLabel.text = displayResaltLabel.text! + number
+            }
+        }else {
+            displayResaltLabel.text = number
+            stillTyping = true
+        }
+      }
+    @IBAction func twoOperandSignPressed(_ sender: UIButton) {
+        
+        firstOperand = currentInput
+        print(firstOperand)
+        stillTyping = false
+        
     }
     
     }
